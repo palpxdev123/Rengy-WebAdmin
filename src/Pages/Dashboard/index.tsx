4fbfd7e3723eae9efae177aef0bd2721d24564a1
palpx-dev6 <dev6github.palpx@gmail.com>
2025-09-22 15:36:25 +0530

    cardcomponent

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import PageLayout from "../../Components/PageLayout";
 import "../../Styles/index.scss";
+import FlatCard from "./Components/Cards";
 import './style.scss';
 const Dashboard = () => {
   return (
     <PageLayout title={"tatat"}>
       <p>hi</p>
+      <FlatCard value={1765} percentage={-8.2} label="New Signups"/>
     </PageLayout>
   )
 }
